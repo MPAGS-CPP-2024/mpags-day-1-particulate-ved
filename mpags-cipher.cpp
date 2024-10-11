e3c58f6309b8392352b188e53f91aa78e94e6c14
Vedanshu Mahajan <cp24131@bristol.ac.uk>
2024-10-11 12:00:55 +0000

    Updated mpags-cipher

---
+++ mpags-cipher.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    //Placeholder comment
+
+
+    std::string msg{"Hello World!"};
+
+    
+    std::cout << msg << std::endl;
+    std::cout << msg[4] << std::endl;
+}
